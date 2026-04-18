86f4c24ed58a3b4f82588217f9e4b6fba80d0a0c
Joykrisno <97521673+Joykrisno@users.noreply.github.com>
2026-04-18 13:48:14 +0600

    Add project files.

---
+++ Duha-Global-Technology-WebSite/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Duha_Global_Technology_WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Duha_Global_Technology_WebSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Duha-Global-Technology-WebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Duha_Global_Technology_WebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
