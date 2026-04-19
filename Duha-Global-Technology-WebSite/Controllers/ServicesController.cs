9caef64a17e9c4ad75810eac3926c9ef03be2545
Joykrisno <97521673+Joykrisno@users.noreply.github.com>
2026-04-19 09:57:40 +0600

    added

---
+++ Duha-Global-Technology-WebSite/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Duha_Global_Technology_WebSite.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
